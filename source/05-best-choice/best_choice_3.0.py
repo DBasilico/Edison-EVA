9b790a1f6d98cac940fbfb6239a71e29a4edf7b7
dbasilico <7CcLYdfQfp2Wa7i>
2023-11-14 09:38:12 +0100

    Adeguamenti best-choice

--- source/05-best-choice/best_choice_3.0.py
+++ source/05-best-choice/best_choice_3.0.py
@@ -12,15 +12,17 @@ import pandas as pd
 from dateutil.relativedelta import relativedelta
 from lib_best_choice import extract_1g_giorno, extract_1g_best, extract_2g_best, build_recupero_na, delete_folder_s3
 
-COD_SIMULAZIONE = sys.argv[1]
 NOME_TABELLA = 'BEST_CHOICE'
 NOME_BUCKET = 'eva-qa-s3-model'
-TRACE_ANALISI = eval(sys.argv[2])
 
+COD_SIMULAZIONE = sys.argv[1]
+TRACE_ANALISI = eval(sys.argv[2])
 INIZIO_PERIODO = datetime.strptime(sys.argv[3], '%Y-%m-%d').date()
 FINE_PERIODO = datetime.strptime(sys.argv[4], '%Y-%m-%d').date()
-
 EFFETTURARE_RECUPERO = eval(sys.argv[5])
+GIORNI_INDIETRO_RECUPERO = int(sys.argv[6])
+EFFETTUARE_PREVISIONE = eval(sys.argv[7])
+LAG_PREVISIONE = int(sys.argv[8]) if EFFETTUARE_PREVISIONE else 0
 
 tab_path = f'datamodel/{NOME_TABELLA}/'
 
@@ -33,7 +35,6 @@ spark = SparkSession.builder.appName('EVA - BEST CHOICE').enableHiveSupport().ge
 #############################
 
 if EFFETTURARE_RECUPERO:
-    GIORNI_INDIETRO_RECUPERO = int(sys.argv[6])
     INIZIO_PERIODO_RECUPERO = INIZIO_PERIODO - relativedelta(days=1+GIORNI_INDIETRO_RECUPERO)
     FINE_PERIODO_RECUPERO = INIZIO_PERIODO - relativedelta(days=1)
     df_recupero_na = spark.read.parquet(
@@ -66,6 +67,7 @@ if not df_conf_cluster.index.is_unique:
 # POD attivi in cui dobbiamo calcolare la BEST CHOICE (DATA FORNITURA)
 df_anagrafica = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/ANAG_SII')
 df_anagrafica = df_anagrafica.filter(f.col('TRATTAMENTO') == 'O')
+df_anagrafica = df_anagrafica.withColumn('TIPO_FLUSSO', f.when(f.col('TIPO_MISURATORE') == 'G', f.lit('2G')).otherwise(f.lit('1G')))
 df_anagrafica = df_anagrafica.withColumn('DT_INI_VALIDITA', f.to_timestamp(f.col('DT_INI_VALIDITA'), 'dd/MM/yyyy HH:mm:SS'))
 df_anagrafica = df_anagrafica.withColumn('DT_FIN_VALIDITA', f.to_timestamp(f.col('DT_FIN_VALIDITA'), 'dd/MM/yyyy HH:mm:SS'))
 df_anagrafica = df_anagrafica.withColumn('CONSUMO_ANNUO_COMPLESSIVO', f.regexp_replace(f.col('CONSUMO_ANNUO_COMPLESSIVO'), ',', '.').cast(DoubleType()))
@@ -76,15 +78,31 @@ df_anagrafica = df_anagrafica.filter(
     (f.col('DT_INI_VALIDITA') <= now)
 )
 
-# CODIZIONI PER CAPIRE SE POD E' 1G OPPURE 2G
-df_anagrafica = df_anagrafica.withColumn('FLUSSO_DATI', f.when(
-    f.col('TIPO_MISURATORE') == 'G',
-    f.lit('2G')
-).otherwise(
-    f.lit('1G')
-))
-
 # TODO: gestione degli errori in anagrafica
+
+##########################
+### ANAGRAFICA PER EAC ###
+##########################
+
+df_eac = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/ANAG_SII')
+df_eac = df_eac.filter(f.col('TRATTAMENTO') == 'O')
+df_eac = df_eac.withColumn('DT_INI_VALIDITA', f.to_date(f.col('DT_INI_VALIDITA'), 'dd/MM/yyyy HH:mm:SS'))
+df_eac = df_eac.withColumn('DT_FIN_VALIDITA', f.to_date(f.col('DT_FIN_VALIDITA'), 'dd/MM/yyyy HH:mm:SS'))
+df_eac = df_eac.withColumn('CONSUMO_ANNUO_COMPLESSIVO', f.regexp_replace(f.col('CONSUMO_ANNUO_COMPLESSIVO'), ',', '.').cast(DoubleType()))
+
+df_eac = df_eac.groupBy('POD', 'DT_INI_VALIDITA', 'DT_FIN_VALIDITA').agg(f.avg('CONSUMO_ANNUO_COMPLESSIVO').alias('EAC'))
+df_eac = df_eac.withColumn('DATA', f.expr('sequence(DT_INI_VALIDITA, DT_FIN_VALIDITA, interval 1 day)'))
+df_eac = df_eac.withColumn('NUMERO_GIORNI_PERIODO', f.size(f.col('DATA')))
+df_eac = df_eac.withColumn('DATA', f.explode('DATA'))
+df_eac = df_eac.withColumn('EAC', f.col('EAC')/f.col('NUMERO_GIORNI_PERIODO')).drop('NUMERO_GIORNI_PERIODO')
+
+df_num_ore_giorno = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/CALENDARIO_GME').groupBy('DATA').count().withColumnRenamed('count', 'NUM_ORE_GIORNO')
+
+df_eac = df_eac.join(df_num_ore_giorno, on=['DATA'], how='left')
+
+df_eac = df_eac.withColumn('EAC', f.col('EAC')/f.col('NUM_ORE_GIORNO')).drop('NUM_ORE_GIORNO')
+
+df_eac = df_eac.groupBy('POD', 'DATA').agg(f.avg('EAC').alias('EAC'))
 
 ###############
 ### CLUSTER ###
@@ -93,14 +111,12 @@ df_anagrafica = df_anagrafica.withColumn('FLUSSO_DATI', f.when(
 df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.lit(None).cast(StringType()))
 
 for k_rule in df_conf_cluster.index.to_numpy():
-
     row = df_conf_cluster.loc[int(k_rule)]
     lista_pod_singoli = row['LISTA_POD_SINGOLI']
     cluster_pod_singoli = row['CLUSTER_POD_SINGOLI']
     cluster_pod_singoli_nome = row['CLUSTER_POD_SINGOLI_NOME']
     regola_select = row['REGOLA_SELECT']
     regola_where = row['REGOLA_WHERE']
-
     if (lista_pod_singoli is not None) and (
             (cluster_pod_singoli is not None) or (cluster_pod_singoli_nome is not None) or (
             regola_select is not None) or (regola_where is not None)):
@@ -115,46 +131,33 @@ for k_rule in df_conf_cluster.index.to_numpy():
     if (regola_select is not None) and ((lista_pod_singoli is not None) or (cluster_pod_singoli is not None) or (
             cluster_pod_singoli_nome is not None) or (regola_select is None)):
         raise AttributeError('ERRORE: presente REGOLA SELECT assieme a informazioni incompatibili')
-
     if lista_pod_singoli is not None:
         concat_syntax = f.concat_ws('#',
                                     f.coalesce(f.col('ZONA'), f.lit('')),
                                     f.coalesce(f.col('ID_AREA_GESTIONALE'), f.lit('')),
-                                    f.coalesce(f.col('PROVINCIA_CRM'), f.lit('')),
-                                    f.coalesce(f.col('FLUSSO_DATI'), f.lit('')),
+                                    f.lit(''),
+                                    f.lit(''),
                                     f.coalesce(f.col('POD'), f.lit(''))
                                     )
         df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.when(f.col('POD').isin(lista_pod_singoli),
                                                                    concat_syntax).otherwise(f.col('CLUSTER')))
-
     elif cluster_pod_singoli is not None:
         df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.when(f.col('POD').isin(cluster_pod_singoli),
                                                                    f.lit(cluster_pod_singoli_nome)).otherwise(
             f.col('CLUSTER')))
-
     else:
         if not pd.isna(regola_where):
-            df_anagrafica = df_anagrafica.filter(regola_where)
-        df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.expr(row['REGOLA_SELECT']))
+            df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.expr(f'CASE WHEN {regola_where} THEN {regola_select} ELSE CLUSTER END'))
+        else:
+            df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.expr(regola_select))
 
-df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.when(
-    ((f.col('CLUSTER').isNull()) & (f.col('FLUSSO_DATI') == '1G')), f.lit('CLUSTER_MANCANTI_1G')
-).when(
-    ((f.col('CLUSTER').isNull()) & (f.col('FLUSSO_DATI') == '2G')), f.lit('CLUSTER_MANCANTI_2G')
-).when(
-    ((f.col('CLUSTER').isNull()) & (~(f.col('FLUSSO_DATI').isin(['1G', '2G'])))), f.lit('CLUSTER_MANCANTI')
-).otherwise(
-    f.col('CLUSTER')
-))
+df_anagrafica = df_anagrafica.withColumn('CLUSTER', f.coalesce(f.col('CLUSTER'), f.lit('CLUSTER_MANCANTI')))
+
+df_anagrafica.repartition(1).write.mode('overwrite').parquet(f's3://{NOME_BUCKET}/ANAGRAFICA_CLUSTER')
 
 df_anagrafica = df_anagrafica.select(
     f.col('POD'),
-    f.col('CLUSTER'),
-    f.col('FLUSSO_DATI'),
-    f.col('CONSUMO_ANNUO_COMPLESSIVO').alias('EAC'),
-    f.col('ZONA'),
-    f.col('ID_AREA_GESTIONALE'),
-    f.col('PROVINCIA_CRM')
+    f.col('CLUSTER')
 )
 
 # TODO: eliminare drop duplicates: non dovrebbero esistere pod doppi
@@ -165,7 +168,7 @@ df_anagrafica = df_anagrafica.dropDuplicates(subset=['POD'])
 ######################
 
 df_calendario = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/CALENDARIO_GME') \
-    .filter(f.col('DATA').between(INIZIO_PERIODO, FINE_PERIODO)) \
+    .filter(f.col('DATA').between(INIZIO_PERIODO, FINE_PERIODO + relativedelta(days=LAG_PREVISIONE))) \
     .select(
         'UNIX_TIME',
         'TIMESTAMP',
@@ -174,24 +177,19 @@ df_calendario = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/CALENDARIO_GME
         'GIORNO_SETTIMANA'
 )
 
-df_num_giorni_anno = spark.read.parquet(f's3://{NOME_BUCKET}/datamodel/CALENDARIO_GME') \
-    .groupBy('ANNO').count().withColumnRenamed('count', 'NUM_GIORNO_ORA')
+OFFSET = df_calendario.filter(f.col('DATA').between(FINE_PERIODO+relativedelta(days=1), FINE_PERIODO+relativedelta(days=LAG_PREVISIONE))).count()
 
 df_best_choice = df_anagrafica.crossJoin(df_calendario)
-df_best_choice = df_best_choice.withColumn('ANNO', f.year(f.col('DATA')))
-df_best_choice = df_best_choice.join(df_num_giorni_anno, on=['ANNO'], how='left')
-df_best_choice = df_best_choice.withColumn('EAC_GIORNO_ORA', f.col('EAC') / f.col('NUM_GIORNO_ORA')) \
-    .drop('NUM_GIORNO_ORA', 'ANNO')
 
-# Windows function per recupero dati mancanti
-w_recupero_na = Window.partitionBy('POD', 'ORA_GME').orderBy(f.col('DATA').desc()).rowsBetween(Window.currentRow, Window.unboundedFollowing)
+df_best_choice = df_best_choice.join(df_eac, on=['POD', 'DATA'], how='left')
 
 ##########################
 ### MISURE 1G - GIORNO ###
 ##########################
 
 df_1g_giorno = extract_1g_giorno(spark, INIZIO_PERIODO, FINE_PERIODO)
-df_best_choice = df_best_choice.join(df_1g_giorno, on=['POD', 'DATA', 'ORA_GME', 'FLUSSO_DATI'], how='left')
+df_best_choice = df_best_choice.join(df_1g_giorno, on=['POD', 'DATA', 'ORA_GME'], how='left')
+df_1g_giorno.write.mode('overwrite').parquet(f's3://{NOME_BUCKET}/1G_GIORNO')
 del df_1g_giorno
 
 ########################
@@ -199,7 +197,8 @@ del df_1g_giorno
 ########################
 
 df_1g_best = extract_1g_best(spark, INIZIO_PERIODO, FINE_PERIODO)
-df_best_choice = df_best_choice.join(df_1g_best, on=['POD', 'DATA', 'ORA_GME', 'FLUSSO_DATI'], how='left')
+df_best_choice = df_best_choice.join(df_1g_best, on=['POD', 'DATA', 'ORA_GME'], how='left')
+df_1g_best.write.mode('overwrite').parquet(f's3://{NOME_BUCKET}/1G_BEST')
 del df_1g_best
 
 ########################
@@ -207,7 +206,8 @@ del df_1g_best
 ########################
 
 df_2g_best = extract_2g_best(spark, INIZIO_PERIODO, FINE_PERIODO)
-df_best_choice = df_best_choice.join(df_2g_best, on=['POD', 'DATA', 'ORA_GME', 'FLUSSO_DATI'], how='left')
+df_best_choice = df_best_choice.join(df_2g_best, on=['POD', 'DATA', 'ORA_GME'], how='left')
+df_2g_best.write.mode('overwrite').parquet(f's3://{NOME_BUCKET}/2G_BEST')
 del df_2g_best
 
 ###########################
@@ -220,65 +220,45 @@ else:
     df_best_choice = df_best_choice.withColumn('ENERGIA_1G_RECUPERO_NA', f.lit(None).cast(DoubleType()))
     df_best_choice = df_best_choice.withColumn('ENERGIA_2G_RECUPERO_NA', f.lit(None).cast(DoubleType()))
 
-df_best_choice = df_best_choice.withColumn('ENERGIA_1G_BEST_VALORE', f.when(
-    f.col('ENERGIA_1G_BEST').isNotNull(), f.col('ENERGIA_1G_BEST')
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('ENERGIA_1G_GIORNO_VALORE', f.when(
-    (f.col('ENERGIA_1G_BEST').isNull()) & (f.col('ENERGIA_1G_GIORNO').isNotNull()),
-    f.col('ENERGIA_1G_GIORNO')
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('ENERGIA_2G_BEST_VALORE', f.when(
-    f.col('ENERGIA_2G_BEST').isNotNull(),
-    f.col('ENERGIA_2G_BEST')
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('ENERGIA_NA_VALORE', f.when(
-    ((f.col('FLUSSO_DATI') == '1G') & (f.col('ENERGIA_1G_BEST').isNull()) & (f.col('ENERGIA_1G_GIORNO').isNull())),
-    f.col('ENERGIA_1G_RECUPERO_NA')
-).when(
-    ((f.col('FLUSSO_DATI') == '2G') & (f.col('ENERGIA_2G_BEST').isNull())),
-    f.col('ENERGIA_1G_RECUPERO_NA')
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('ENERGIA_EAC_VALORE', f.when(
-    ((f.col('FLUSSO_DATI') == '1G') & (f.col('ENERGIA_1G_BEST').isNull()) & (f.col('ENERGIA_1G_GIORNO').isNull()) & (f.col('ENERGIA_NA_VALORE').isNull())) |
-    ((f.col('FLUSSO_DATI') == '2G') & (f.col('ENERGIA_2G_BEST').isNull()) & (f.col('ENERGIA_NA_VALORE').isNull())),
-    f.col('EAC_GIORNO_ORA')
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('CONSUMI', f.when(
-    f.col('FLUSSO_DATI') == '1G',
-    f.coalesce(f.col('ENERGIA_1G_BEST'), f.col('ENERGIA_1G_GIORNO'), f.col('ENERGIA_1G_RECUPERO_NA'), f.col('EAC_GIORNO_ORA'))
-).when(
-    f.col('FLUSSO_DATI') == '2G',
-    f.coalesce(f.col('ENERGIA_2G_BEST'), f.col('ENERGIA_2G_RECUPERO_NA'), f.col('EAC_GIORNO_ORA'))
-).otherwise(
-    f.lit(None)
-))
-
-df_best_choice = df_best_choice.withColumn('FLAG_DATO_MANCANTE', f.when(
-    ((f.col('FLUSSO_DATI') == '1G') & (f.col('ENERGIA_1G_BEST').isNull()) & (f.col('ENERGIA_1G_GIORNO').isNull())) |
-    ((f.col('FLUSSO_DATI') == '2G') & (f.col('ENERGIA_2G_BEST').isNull())),
+df_best_choice = df_best_choice.withColumn('IS_MISSING', f.when(
+    (f.col('ENERGIA_1G_BEST').isNull()) & (f.col('ENERGIA_1G_GIORNO').isNull()) & (f.col('ENERGIA_2G_BEST').isNull()),
     f.lit(1)
 ).otherwise(
     f.lit(0)
 ))
 
+df_best_choice = df_best_choice.withColumn('ENERGIA_NA_VALORE', f.when(
+    f.col('IS_MISSING') == 1,
+    f.coalesce(f.col('ENERGIA_1G_RECUPERO_NA'), f.col('ENERGIA_2G_RECUPERO_NA'))
+).otherwise(
+    f.lit(None)
+))
+
+df_best_choice = df_best_choice.withColumn('ENERGIA_EAC_VALORE', f.when(
+    (f.col('IS_MISSING') == 1) & (f.col('ENERGIA_NA_VALORE').isNull()),
+    f.col('EAC')
+).otherwise(
+    f.lit(None)
+))
+
+df_best_choice = df_best_choice.withColumn('CONSUMI', f.coalesce(
+    f.col('ENERGIA_1G_BEST'),
+    f.col('ENERGIA_1G_GIORNO'),
+    f.col('ENERGIA_2G_BEST'),
+    f.col('ENERGIA_1G_RECUPERO_NA'),
+    f.col('ENERGIA_2G_RECUPERO_NA'),
+    f.col('EAC')
+))
+
+window_lag = Window.partitionBy('POD').orderBy(f.col('UNIX_TIME').asc())
+
+df_best_choice = df_best_choice.withColumn('PREVISIONE', f.lag(f.col('CONSUMI'), offset=OFFSET).over(window_lag))
+
 #############################################
 ### SCRITTURA TABELLA ANALISI BEST-CHOICE ###
 #############################################
+
+df_best_choice.write.mode('overwrite').parquet(f's3://{NOME_BUCKET}/datamodel/{NOME_TABELLA}_ANALISI')
 
 if TRACE_ANALISI:
     outpath_trace = f's3://{NOME_BUCKET}/datamodel/{NOME_TABELLA}_ANALISI'
@@ -294,12 +274,24 @@ if TRACE_ANALISI:
 ### AGGREGAZIONE CLUSTER ###
 ############################
 
-df_best_choice = df_best_choice.groupBy('CLUSTER', 'DATA', 'UNIX_TIME', 'ORA_GME', 'TIMESTAMP', 'FLUSSO_DATI').agg(
+if EFFETTUARE_PREVISIONE:
+    df_best_choice_previsione = df_best_choice.filter(f.col('DATA').between(FINE_PERIODO+relativedelta(days=1), FINE_PERIODO+relativedelta(days=LAG_PREVISIONE)))
+    df_best_choice = df_best_choice.filter(f.col('DATA').between(INIZIO_PERIODO, FINE_PERIODO))
+
+    df_best_choice_previsione = df_best_choice_previsione.groupBy('CLUSTER', 'UNIX_TIME').agg(
+        f.sum('PREVISIONE').alias('PREVISIONE')
+    )
+    df_best_choice_previsione = df_best_choice_previsione.withColumn('NOME_TRANSFORM', f.lit('Benchmark'))
+    df_best_choice_previsione = df_best_choice_previsione.withColumn('SIMULAZIONE', f.lit(COD_SIMULAZIONE))
+
+    df_best_choice_previsione.write.mode('overwrite').parquet('s3://eva-qa-s3-model/datamodel/FORECAST_SAGEMAKER/BENCHMARK')
+
+df_best_choice = df_best_choice.groupBy('CLUSTER', 'DATA', 'UNIX_TIME', 'ORA_GME', 'TIMESTAMP').agg(
     f.count(f.lit(1)).cast('bigint').alias('N_POD'),
-    f.sum('FLAG_DATO_MANCANTE').cast('bigint').alias('N_POD_MANCANTI'),
-    f.sum('ENERGIA_1G_BEST_VALORE').alias('CONSUMI_1G_BEST'),
-    f.sum('ENERGIA_1G_GIORNO_VALORE').alias('CONSUMI_1G_GIORNO'),
-    f.sum('ENERGIA_2G_BEST_VALORE').alias('CONSUMI_2G_BEST'),
+    f.sum('IS_MISSING').cast('bigint').alias('N_POD_MANCANTI'),
+    f.sum('ENERGIA_1G_BEST').alias('CONSUMI_1G_BEST'),
+    f.sum('ENERGIA_1G_GIORNO').alias('CONSUMI_1G_GIORNO'),
+    f.sum('ENERGIA_2G_BEST').alias('CONSUMI_2G_BEST'),
     f.sum('ENERGIA_NA_VALORE').alias('CONSUMI_NA_RECUPERATI'),
     f.sum('ENERGIA_EAC_VALORE').alias('CONSUMI_NA_EAC'),
     f.sum('CONSUMI').alias('CONSUMI')
@@ -315,7 +307,7 @@ while data_corrente <= FINE_PERIODO:
     data_corrente += relativedelta(days=1)
 del data_corrente
 
-df_best_choice.repartition(50, 'CLUSTER').write.mode('append').partitionBy(['SIMULAZIONE', 'DATA']).parquet(output_table)
+df_best_choice.write.mode('append').partitionBy(['SIMULAZIONE', 'DATA']).parquet(output_table)
 
 if df_recupero_na is not None:
-   delete_folder_s3(bucket=NOME_BUCKET, folder_path=df_recupero_na)
+    delete_folder_s3(bucket=NOME_BUCKET, folder_path=df_recupero_na)
